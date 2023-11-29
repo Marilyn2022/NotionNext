47f585cf312648842d37366dbf8c559ddd9fcf72
Marilyn2022 <111874848+Marilyn2022@users.noreply.github.com>
2023-11-29 10:51:36 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,26 +1,26 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-07-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://www.205066.xyz' },
+    { title: '访问文档中心获取更多帮助', url: 'https://www.205066.xyz' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '精选软件',
+  HEO_HERO_TITLE_2: '免费白嫖',
+  HEO_HERO_TITLE_3: 'www.203511.xyz',
+  HEO_HERO_TITLE_4: '影音软件',
+  HEO_HERO_TITLE_5: 'TVbox、调试助手、影视仓持续更新',
+  HEO_HERO_TITLE_LINK: 'https://www.205066.xyz',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '影音软件', url: '/tag/TVbox' },
+  HEO_HERO_CATEGORY_2: { title: 'ChatGPT场景应用', url: '/tag/ChatGPT场景应用' },
+  HEO_HERO_CATEGORY_3: { title: '网站搭建', url: '/tag/网站搭建' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -29,13 +29,13 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+   '你好！我是',
+    '🔍 AIGC类产品',
+    '🤝 TVbox、调试助手、影视仓更新',
+    '🏃 电脑端黑科技软件',
+    '🏠 网站搭建教程',
+    '🤖️ 手机实用工具',
+    '🧱 其它好玩工具'
   ],
   HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
 
@@ -103,7 +103,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://www.205066.xyz',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
